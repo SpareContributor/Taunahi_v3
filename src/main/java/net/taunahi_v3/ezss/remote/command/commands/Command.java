db272fff282e718a0c98b467ac031e790e1d5a30
Diana <165363101+SpareContributor@users.noreply.github.com>
2024-05-28 02:16:42 +0700

    update src <3

--- src/main/java/net/taunahi_v3/ezss/remote/command/commands/Command.java
+++
@@ -1,9 +0,0 @@
-package net.taunahi_v3.ezss.remote.command.commands;
-
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-
-@Retention(RetentionPolicy.RUNTIME)
-public @interface Command {
-    String label();
-}
